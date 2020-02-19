744a9a0c0258392e1d6aaac3cd649a03511002a0
LupusMs <msvyrydovych@gmail.com>
2020-02-19 20:11:07 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://expenses-4c37.restdb.io/rest/february-2020');
+xhr.setRequestHeader('x-apikey', apikey);
+xhr.responseType = 'json';
+xhr.onload = function () {
+    if (xhr.status != 200) { 
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); 
+    } else { 
+        var items = [];
+        var data = this.response[0];
+        for ( const item in data ) {
+            if (item != "_id") {
+                items.push(item + " : " + data[item] + " ");
+            }
+        }
+        document.getElementById("test").innerHTML = items.join("");
+    }
+};
+xhr.send();  
+  
